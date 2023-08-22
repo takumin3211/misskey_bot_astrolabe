87a504ddea9b968b13c1c1b10974094c05bc7d08
takumin3211 <enerc01@outlook.com>
2023-08-22 21:20:13 +0900

    test

---
+++ misskey_bot_astrolabe/settings.py
@@ -0,0 +1,11 @@
+﻿text = "test.txt"
+dbname = 'api.db'
+csvname = 'api.csv'
+AI_NAME = 'Astrolabe'
+AI_ID = '"9ib6lbdave'
+TOKEN = 'UeZEaJ09jR9FKI7NHh5vtMaNYKfBxR0a'
+ADRESS = 'misskey.seitendan.com'
+
+USER_NAME = 'test_name'
+
+LLMPATH = "./model/chronos-13b.ggmlv3.q4_K_M.bin"
